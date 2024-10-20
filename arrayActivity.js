582e5b1e68abd936988d2e9f0aa67fda7fc365eb
unknown <ceedriclazaro010205@gmail.com>
2024-10-20 05:05:28 -0800

    Initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+let favSingers = ["Adele", "Ed Sheeran", "Beyoncé"];
+
+console.log("First favorite singer: " + favSingers[0]);
+
+let favNumbers = [7, 22, 8, 3];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("First item in mixedArr: " + mixedArr[0]);       
+console.log("Second item in mixedArr: ", mixedArr[1]);       
+console.log("Third item in mixedArr: " + mixedArr[2]);       
+console.log("Fourth item in mixedArr: " + mixedArr[3]);      
